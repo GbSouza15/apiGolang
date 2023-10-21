fa38c1460738326cdbbb6a19dae9bde5875f850d
Gabriel <gabriel.souza2004@outlook.com.br>
2023-10-20 23:04:20 -0300

    feat: add handler func Home and configure router

--- api/routes/router.go
+++ api/routes/router.go
@@ -1,1 +1,11 @@
 package api
+
+import (
+	"net/http"
+
+	api "github.com/GbSouza15/apiGolang/api/handlers"
+)
+
+func RoutesApi() {
+	http.HandleFunc("/", api.HomeHandler)
+}
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	api "github.com/GbSouza15/apiGolang/api/routes"
 )
 
 func main() {
+
+	api.RoutesApi()
 
 	fmt.Println("Server is running on port 8080")
 
